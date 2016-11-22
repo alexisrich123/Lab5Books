76a9f186e6224b0eb0db4cc749570287d3fe374f
SK207_24 <SK207_24@K207_24.carrollcc.edu>
2016-11-21 20:48:58 -0500

    WORKING

--- Books/src/alexis/CreatDB.java
+++ Books/src/alexis/CreatDB.java
@@ -14,6 +14,7 @@ Write a program that allows a user to insert new books and/or delete existing bo
 Write a program that allows a user to list all books filtered by the user inputted author, genre, and/or year published.
  Display the sum of the pages of the books filtered by the user.
 */
+// USE H BOX WITH ARCHER PANES FX
 public class CreatDB {
 
 	public static void main(String[] args) {
@@ -23,18 +24,64 @@ public class CreatDB {
 			Connection conn= DriverManager.getConnection(DB_URL);
 			//  get rid of the table is exists
 			dropTable(conn);
+			//making table
+			buildBookTable(conn);
+			// SUM OF BOOK PAGE
+			//sumofpages(conn);
 	
+		
+			
+	conn.close();
 		}
 		catch(SQLException ex ){
-			
+			ex.printStackTrace();
 		}
 	}
 	
-	public static void dropTable(Connection conn){
+
+
+
+
+
+	/*private static void sumofpages(Connection conn) {
+
+	try {
+			Statement stmt=conn.createStatement();
+			stmt.executeQuery("SELECT NumberOfPages SUM";
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+	}
+*/
+
+
+
+
+
+	private static void buildBookTable(Connection conn) {
 		try{
-		Statement stamt= conn.createStatement();
+			Statement stmt=conn.createStatement();
+			stmt.execute("CREATE TABLE BOOKS("+"Title CHAR(35),"+"Author CHAR(40),"+
+			"Genre CHAR(20),"+" YearPublished INTEGER,"+" NumberOfPages INTEGER"+")");
+			stmt.executeQuery("INSERT INTO BOOKS VALUES('Hamlet', 'Shakespeare','tragedy',1599,120)");
+			System.out.println("A BOOK WAS INSRETED");
 		}
 		catch(SQLException ex){
+			System.out.println("ERROR:"+"Book was no put away");
+		}
+	}
+
+	public static void dropTable(Connection conn){
+		try{
+		Statement stmt= conn.createStatement();
+		// drop table comman,update ,delete
+		stmt.executeQuery("DROP TABLE BOOKS");
+		System.out.println("table drop");
+		}
+		catch(SQLException ex){
+			System.out.println("ERROR");
 			
 		}
 	}
---
+++ Books/src/alexis/PutAway.java
@@ -0,0 +1,19 @@
+package alexis;
+
+import java.sql.Connection;
+
+public class PutAway {
+
+	public static void main(String[] args) {
+
+	
+			String sql="INSERT INTO BOOK"
+									+"(Title,Author,Genre, YearPublished, NumberOfPages)"
+									+"VALUES"
+									+"(?,?,?);";
+			
+		
+
+	}
+
+}
